2fed967c8fe8923d84d55ec065bedf441801f523
SrAxado <sr.axado@gmail.com>
2024-08-23 23:52:39 +0100

    added size unit

--- exerc13.cpp
+++ exerc13.cpp
@@ -14,17 +14,17 @@ int main() {
   float myFloat = 1.0;
   double myDouble = 1.0;
 
-  cout << "The size of a bool is " << sizeof(myBool) << ".\n";
-  cout << "The size of an int is " << sizeof(myInt) << ".\n";
-  cout << "The size of an unsigned int is " << sizeof(myUnsignedInt) << ".\n";
-  cout << "The size of a short int is " << sizeof(myShortInt) << ".\n";
-  cout << "The size of a short unsigned short int is " << sizeof(myUnsignedShortInt) << ".\n";
-  cout << "The size of a long int is " << sizeof(myLongInt) << ".\n";
-  cout << "The size of an unsigned long int is " << sizeof(myUnsignedLongInt) << ".\n";
-  cout << "The size of a char is " << sizeof(myChar) << ".\n";
-  cout << "The size of an unsigned char is" << sizeof(myUnsignedChar) << ".\n";
-  cout << "The size of a float is " << sizeof(myFloat) << ".\n";
-  cout << "The size of double is " << sizeof(myDouble) << ".\n";
+  cout << "The size of a bool is " << sizeof(myBool) << " bytes.\n";
+  cout << "The size of an int is " << sizeof(myInt) << " bytes.\n";
+  cout << "The size of an unsigned int is " << sizeof(myUnsignedInt) << " bytes.\n";
+  cout << "The size of a short int is " << sizeof(myShortInt) << " bytes.\n";
+  cout << "The size of a short unsigned short int is " << sizeof(myUnsignedShortInt) << " bytes.\n";
+  cout << "The size of a long int is " << sizeof(myLongInt) << " bytes.\n";
+  cout << "The size of an unsigned long int is " << sizeof(myUnsignedLongInt) << " bytes.\n";
+  cout << "The size of a char is " << sizeof(myChar) << " bytes.\n";
+  cout << "The size of an unsigned char is" << sizeof(myUnsignedChar) << " bytes.\n";
+  cout << "The size of a float is " << sizeof(myFloat) << " bytes.\n";
+  cout << "The size of double is " << sizeof(myDouble) << " bytes.\n";
 
   return 0;
 }
