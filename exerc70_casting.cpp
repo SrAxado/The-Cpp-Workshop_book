1f6df2e49e6dad181a93242589e564dc58a2ee80
SrAxado <sr.axado@gmail.com>
2025-09-24 15:42:01 +0100

    small experiments in the exerc70

--- exerc70_casting.cpp
+++ exerc70_casting.cpp
@@ -4,15 +4,17 @@
 class Shape {
   public:
     virtual int CalculateArea() = 0;
+    int coiso;
 
   protected:
-    int area = 0;
+    int area = 2;
 };
 
 class Square : public Shape {
   public:
-    int height = 0;
+    int height = 1;
     int CalculateArea() override {
+      std::cout << "Calculating... ";
       area = height * height;
       return area;
     }
@@ -31,11 +33,12 @@ class Circle : public Shape {
 int main() {
 
   Shape* square = new Square();
-  std::cout << "Square Area: " << square->CalculateArea() << std::endl;
+  std::cout << "Square Area: " << square->CalculateArea() << std::endl; // we can only access the members defined in Shape
+  // in this case, being a virtual method, we access the overrider method
 
   Square* square2 = static_cast<Square*>(square);
-  square2->height = 10;
-  std::cout << "Square Area: " << square->CalculateArea() << std::endl;
+  square2->height = 10; // we have access to the members of the Square and Shape
+  std::cout << "Square Area: " << square->CalculateArea() << std::endl; // still accessing the overrider method
 
 
   Shape* circle = new Circle();
@@ -53,9 +56,28 @@ int main() {
   // shape3->area = 3; // error: "Shape::area" is inaccessible
   // we're only able to access members declared in the base class
   std::cout << "Circle's Shape Area: " << shape3->CalculateArea() << std::endl;
+  shape3->coiso = 25;
 
   delete square;
   square = nullptr;
   delete circle;
   circle = nullptr;
+  // delete square2;
+  // square2 = nullptr;
+  // delete circle2;
+  // circle2 = nullptr;
+  // delete circle3;
+  // circle3 = nullptr;
+  delete shape3;
+  shape3 = nullptr;
 }
+
+/*
+OUTPUT:
+
+Square Area: Calculating... 1
+Square Area: Calculating... 100
+Circle Area: 0
+Circle Area: 314
+Circle's Shape Area: 3
+*/
